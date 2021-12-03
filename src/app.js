e02ee6f16281d52fa2b52144954ec3553bf3a215
jeffreyally <jeffreyally@gmail.com>
2021-12-03 03:19:04 +0000

    Excuse Generator

--- src/app.js
+++ src/app.js
@@ -6,6 +6,32 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  var Animals = [
+    "The thing is, a rabies infested dog ",
+    "The thing is, a rabies infested cat ",
+    "The thing is, a rabies infested raccoon ",
+    "The thing is, a rabies infested fox "
+  ];
+  var Actions = ["broke ", "chewed up ", "ate ", "took "];
+  var Going_Out_Items = [
+    "my phone ",
+    "my wallet ",
+    "my keys ",
+    "my drivers license "
+  ];
+  var When = [
+    "like...como ayer mano. Metí la pata. ",
+    "like...an hour ago bro.",
+    "like...five minutes ago. Crazy right?",
+    "like...literally this morning"
+  ];
+
+  var str = "";
+  str =
+    str +
+    Animals[Math.ceil(Math.random() * 4 - 1)] +
+    Actions[Math.ceil(Math.random() * 4 - 1)] +
+    Going_Out_Items[Math.ceil(Math.random() * 4 - 1)] +
+    When[Math.ceil(Math.random() * 4 - 1)];
+  document.querySelector("#excuse").innerHTML = str;
 };
